b43d242fe865dba2a96378c5631c67d12a004b0a
javidancode <javidanig@code.edu.az>
2022-08-21 23:05:26 +0400

    Added Js-Local, Global variables, Functions

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+// 1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapan bir function yazin.
+
+// // if (n) {
+// //     if (n % 3 == 0 && n % 7 == 0);
+// //     console.log("yes");
+// // } else {
+// //     console.log("no");
+// // }
+
+
+// 3) Her hansisa number elementleri olan arayin icindeki cut ededlerin kvadratlarinin cemini hesablayan bir function yazin.
+
+// let numStr = [2, 4, 6, 8, 12, 14];
+
+// const squareEvenNumbers = (numStr) => {
+//     let sum = 0;
+//     for (let i = 0; i < numStr.length; i++) {
+//         if (numStr[i] % 2 == 0) {
+//             sum = (sum + numStr[i], 2);
+//         }
+
+//     }
+//     return sum;
+// }
+
+// console.log(squareEvenNumbers(numStr));
+
+
+// 4) Daxil ol Gmail ve password duzdu yoxsa sehf
+
+// if (email == " cavid@code.edu.az" && password == "cavid12345") {
+//     console.log("Login Successful!");
+// }
+// else{
+//     console.log("enter again!");
+// }
+
+
+
+// 5) Functiona ededlerden ibaret array gelir, function hemin arraydaki tek ededlerin cemini qaytarmalidir.
+
+// var oddArray = [1,3,5,7,9];
+
+// var sum = 1;
+
+// for (let i = 1; i < oddArray.length; i++) {
+//     if (oddArray[i] == 1) {
+//         sum += oddArray[i];
+//         console.log(sum);
+//     }
+    
+// }
+
+
+// 6) Functiona ededlerden ibaret array gelir, function hemin arraydaki cut ededlerin sayini qaytarmalidir.
+
+// var evenArray = [2,4,6,8];
+
+// var sum = 0;
+
+// for (let i = 0; i < evenArray.length; i++) {
+//     if (evenArray[i] == 0) {
+//         sum += evenArray[i];
+//         console.log(sum);
+//     }
+    
+// }
